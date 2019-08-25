343fb1a4225e90880dd6a37379f5862135ff489f
Jan Gana <jan.gana@hiring-hub.com>
2019-08-25 23:11:01 +0100

    Implement Redux, GET, DELETE, ADD lead

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { getLeads, deleteLead } from '../../actions/leads';
+import PropTypes from 'prop-types';
 
 export class Leads extends Component {
+  componentDidMount() {
+    const { getLeads } = this.props;
+    getLeads();
+  }
+
+  handleDeleteLead = id => {
+    const { deleteLead } = this.props;
+    deleteLead(id);
+  };
+
   render() {
+    const { leads } = this.props;
+
     return (
-      <div>
-        leads list
-      </div>
-    )
+      <Fragment>
+        <h2>Leads</h2>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {leads.map(lead => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button
+                    className='btn btn-danger btn-sm'
+                    onClick={() => this.handleDeleteLead(lead.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
   }
 }
 
-export default Leads;
+Leads.propTypes = {
+  leads: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  getLeads: PropTypes.func.isRequired,
+  deleteLead: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(
+  mapStateToProps,
+  { getLeads, deleteLead }
+)(Leads);
